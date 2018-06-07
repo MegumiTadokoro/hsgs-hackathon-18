49e69c867c64c94b4a43631f4b63163f8314ed02
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2018-06-07 19:45:44 +0700

    Add class Row

---
+++ src/S15/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./index.css";
+
+class Row extends React.Component {
+  render() {
+    const array = [];
+    // for (let id = this.props.N; --id > 0;)
+  }
+}
