81110e985f9d4005c934e652d4e9b80324a0c605
Ngo Tuan Minh <catboom01@gmail.com>
2018-06-08 14:37:26 +0700

    Auto stash before merge of "master" and "origin/master"

--- stories/grad.js
+++ stories/grad.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { 
+	number,
+	withKnobs 
+} from '@storybook/addon-knobs';
 import Board from "../src/S15/index.jsx";
 import Game from "../src/S15/lib/grad.js";
 import ReactGame from "react-gameboard/lib/component";
@@ -7,6 +11,7 @@ import ReactGame from "react-gameboard/lib/component";
 const Grad = ReactGame(Game);
 
 storiesOf("Grad", module)
+	.addDecorator(withKnobs)
   .add("Simple Mode", () => (
     <Grad n={4} m={4}>
       <Board />
@@ -23,8 +28,8 @@ storiesOf("Grad", module)
     </Grad>
   ))
   .add("Custom Mode", () => {
-    const input_n = parseInt(prompt("Please enter the number of rows: "));
-    const input_m = parseInt(prompt("Please enter the number of columns: "));
+		const input_n = number("Row", 4);
+    const input_m = number("Column", 4);
     const n = isNaN(input_n) ? 6 : input_n > 0 ? input_n : 6;
     const m = isNaN(input_m) ? 6 : input_m > 0 ? input_m : 6;
     return (
