02e03e9661a1d756bc45326ddbcf705df84d48c5
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2018-06-08 14:44:16 +0700

    Change to range

--- stories/grad.js
+++ stories/grad.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { 
-	number,
-	withKnobs 
-} from '@storybook/addon-knobs';
+import { number, withKnobs } from "@storybook/addon-knobs";
 import Board from "../src/S15/index.jsx";
 import Game from "../src/S15/lib/grad.js";
 import ReactGame from "react-gameboard/lib/component";
@@ -11,7 +8,7 @@ import ReactGame from "react-gameboard/lib/component";
 const Grad = ReactGame(Game);
 
 storiesOf("Grad", module)
-	.addDecorator(withKnobs)
+  .addDecorator(withKnobs)
   .add("Simple Mode", () => (
     <Grad n={4} m={4}>
       <Board />
@@ -28,10 +25,16 @@ storiesOf("Grad", module)
     </Grad>
   ))
   .add("Custom Mode", () => {
-		const input_n = number("Row", 4);
-    const input_m = number("Column", 4);
-    const n = isNaN(input_n) ? 6 : input_n > 0 ? input_n : 6;
-    const m = isNaN(input_m) ? 6 : input_m > 0 ? input_m : 6;
+    const options = {
+      range: true,
+      step: 1,
+      min: 2,
+      max: 100
+    };
+    const input_n = number("Row", 6, options);
+    const input_m = number("Column", 6, options);
+    const n = input_n > 1 ? input_n : 6;
+    const m = input_m > 1 ? input_m : 6;
     return (
       <Grad n={n} m={m}>
         <Board />
