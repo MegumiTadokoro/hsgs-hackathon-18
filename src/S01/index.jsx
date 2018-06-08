4e7938a1d179d76090be1ea99783b0fc3c8af1e0
cheater2k <vink@Vinh_Dell.localdomain>
2018-06-08 22:49:55 +0700

    Finished GameBoard S01

--- src/S01/index.jsx
+++ src/S01/index.jsx
@@ -4,10 +4,14 @@ import "./index.css"
 
 class Column extends React.Component {
   render() {
-    const array = [];
-    for (let i = 0; i <= 5; ++i) {
-      if (i < 5) array.push(<td key={i} className={this.props.type === 0 ? "s01" : "s01 lastCol"}>{this.props.col[i]}</td>)
-      else array.push(<td key={i} className={this.props.type === 0 ? "s01 lastRow" : "s01 lastRow lastCol"}>{this.props.col[i]}</td>)
+    let array = [];
+    for (let i = 0; i <= this.props.N; ++i) {
+      if (this.props.glow[i] == 1) { // this cell is glowing
+        array.push(<td className={"s01 glow"}>{this.props.col[i]}</td>)
+        continue;
+      }
+      if (i < this.props.N) array.push(<td className={this.props.type == 0 ? "s01" : "s01 lastCol"}>{this.props.col[i]}</td>)
+      else array.push(<td className={this.props.type == 0 ? "s01 lastRow" : "s01 lastRow lastCol"}>{this.props.col[i]}</td>)
     }
     return <tr>{array}</tr>
   }  
@@ -15,85 +19,114 @@ class Column extends React.Component {
 
 class Board extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      A: null, B: null, C: null, D: null, E: null,
-    }
+      value: Array(10).fill(null)
+    };
     
     this.handleChange = this.handleChange.bind(this);
+    this.handleFocus = this.handleFocus.bind(this);
   }
 
   async setStateAsync(state) {
     return new Promise(resolve => this.setState(state, resolve));
   }
 
+  async handleFocus(event) {
+    let value = event.target.value;
+    let name = event.target.name;
+    let arr = this.state.value.slice();
+    arr[name] = value;
+
+    await this.setStateAsync({value: arr});   
+
+    let N = this.props.state.value.length;
+    let variChar = String.fromCharCode(65 + parseInt(name));
+    for (let i = 0; i < N; ++i) {
+      for (let j = 0; j < N; ++j) {
+        if(this.props.state.board[i][j] == variChar) {
+          this.props.state.glow[i][j] = 1; // turn on
+        } else {
+          this.props.state.glow[i][j] = 0; // turn off
+        }
+      }
+    }
+
+    this.props.move({x: this.state});
+  }
+
   async handleChange(event) {
-    const value = event.target.value
-    if(parseInt(value) != value && value != "") return null
-    const name = event.target.name
-    await this.setStateAsync({[name]: value})
-    for(let i = 0; i < 5; ++i) for(let j = 0; j < 5; ++j) {
-      if(this.props.state.board[i][j] === name) {
+    let value = event.target.value;
+    if(parseInt(value) != value && value != "") return null;
+    let name = event.target.name;
+    let arr = this.state.value.slice();
+    arr[name] = value;
+
+    await this.setStateAsync({value: arr});    
+    
+    let N = this.props.state.value.length;
+    let variChar = String.fromCharCode(65 + parseInt(name));
+
+    for (let i = 0; i <= N; ++i) for (let j = 0; j <= N; ++j) {
+      if(this.props.state.board[i][j] == variChar) {
         if(value) {
           if(parseInt(this.props.state.board2[i][j]) == this.props.state.board2[i][j]) {
-            this.props.state.board2[5][j] = parseInt(this.props.state.board2[5][j]) + parseInt(this.props.state.board2[i][j])
-            this.props.state.board2[i][5] = parseInt(this.props.state.board2[i][5]) + parseInt(this.props.state.board2[i][j])
+            this.props.state.board2[N][j] = parseInt(this.props.state.board2[N][j]) + parseInt(this.props.state.board2[i][j]);
+            this.props.state.board2[i][N] = parseInt(this.props.state.board2[i][N]) + parseInt(this.props.state.board2[i][j]);
           }
-          this.props.state.board2[i][j] = value
+          this.props.state.board2[i][j] = value;
           if(parseInt(this.props.state.board2[i][j]) == this.props.state.board2[i][j]) {
-            this.props.state.board2[5][j] = parseInt(this.props.state.board2[5][j]) - parseInt(this.props.state.board2[i][j])
-            this.props.state.board2[i][5] = parseInt(this.props.state.board2[i][5]) - parseInt(this.props.state.board2[i][j])
+            this.props.state.board2[N][j] = parseInt(this.props.state.board2[N][j]) - parseInt(this.props.state.board2[i][j]);
+            this.props.state.board2[i][N] = parseInt(this.props.state.board2[i][N]) - parseInt(this.props.state.board2[i][j]);
           }
         }
         else {
           if(parseInt(this.props.state.board2[i][j]) == this.props.state.board2[i][j]) {
-            this.props.state.board2[5][j] = parseInt(this.props.state.board2[5][j]) + parseInt(this.props.state.board2[i][j])
-            this.props.state.board2[i][5] = parseInt(this.props.state.board2[i][5]) + parseInt(this.props.state.board2[i][j])
+            this.props.state.board2[N][j] = parseInt(this.props.state.board2[N][j]) + parseInt(this.props.state.board2[i][j]);
+            this.props.state.board2[i][N] = parseInt(this.props.state.board2[i][N]) + parseInt(this.props.state.board2[i][j]);
           }
-          this.props.state.board2[i][j] = name
+          this.props.state.board2[i][j] = variChar;
           if(parseInt(this.props.state.board2[i][j]) == this.props.state.board2[i][j]) {
-            this.props.state.board2[5][j] = parseInt(this.props.state.board2[5][j]) - parseInt(this.props.state.board2[i][j])
-            this.props.state.board2[i][5] = parseInt(this.props.state.board2[i][5]) - parseInt(this.props.state.board2[i][j])
+            this.props.state.board2[N][j] = parseInt(this.props.state.board2[N][j]) - parseInt(this.props.state.board2[i][j]);
+            this.props.state.board2[i][N] = parseInt(this.props.state.board2[i][N]) - parseInt(this.props.state.board2[i][j]);
           }
         }
       }
     }
-    this.props.move({x: this.state})
+
+    this.props.move({x: this.state});
   }
 
   render() {
+    let N = this.props.state.value.length;
+
+    // board building
+    let arrBoard = [];
+    for (let i = 0; i <= N; ++i) {
+      arrBoard.push(
+        <Column col={this.props.state.board2[i]} glow={this.props.state.glow[i]} type={i < N ? 0 : 1} N={N}/>
+      );
+    }
+
+    let arrVariable = [];
+    for (let i = 0; i < N; ++i) {
+      let tmp = String.fromCharCode(65 + i);
+      arrVariable.push(
+        <td className="submit-form tab"><span>
+          {tmp} = <input name={i} type="text" maxlength="1" value={this.state.value[i]} onFocus={this.handleFocus} onChange={this.handleChange}/>
+        </span></td>
+      );
+    }
+
     return (
       <div>
-         <table>
+        <table>
           <tbody>
-            <div>
-              <Column col={this.props.state.board2[0]} type={0}/>  
-              <Column col={this.props.state.board2[1]} type={0}/>  
-              <Column col={this.props.state.board2[2]} type={0}/> 
-              <Column col={this.props.state.board2[3]} type={0}/>  
-              <Column col={this.props.state.board2[4]} type={0}/>
-              <Column col={this.props.state.board2[5]} type={1}/>
-            </div>
-          
+            <div>{arrBoard}</div>
+
             <tr><pre>             </pre></tr>
             <div className="submit-form">
-              <tr>
-                <td className="submit-form tab"><span>
-                  A = <input name="A" type="text" maxlength="1" value={this.state.A} onChange={this.handleChange}/>
-                </span></td>
-                <td className="submit-form tab"><span>
-                  B = <input name="B" type="text" maxlength="1" value={this.state.B} onChange={this.handleChange}/>
-                </span></td>
-                <td className="submit-form tab"><span>
-                  C = <input name="C" type="text" maxlength="1" value={this.state.C} onChange={this.handleChange}/>
-                </span></td>
-                <td className="submit-form tab"><span>
-                  D = <input name="D" type="text" maxlength="1" value={this.state.D} onChange={this.handleChange}/>
-                </span></td>
-                <td className="submit-form tab"><span>
-                  E = <input name="E" type="text" maxlength="1" value={this.state.E} onChange={this.handleChange}/>
-                </span></td>
-              </tr>
+              <tr>{arrVariable}</tr>
             </div>
           </tbody>
         </table>
--- stories/S01.js
+++ stories/S01.js
@@ -7,8 +7,18 @@ import ReactGame from "react-gameboard/lib/component";
 const S01 = ReactGame(Game);
 
 storiesOf("S01", module)
+  .add("Easy Mode", () => (
+    <S01 N={4}>
+        <Board/>  
+    </S01>
+  ))
   .add("Medium Mode", () => (
-    <S01>
-       <Board/> 
+    <S01 N={6}>
+        <Board/>  
+    </S01>
+  ))
+  .add("Hard Mode", () => (
+    <S01 N={8}>
+        <Board/>  
     </S01>
   ))
