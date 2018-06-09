9ebf6df81147978d7b3f4392c1d4a78feb1d3e5c
Natsu Kagami <natsukagami@gmail.com>
2018-06-09 09:22:16 +0700

    Re-locate files

--- stories/S17.js
+++ stories/S17.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import Chess from "../knight/index.jsx";
-import Game from "../knight/lib/knight.js";
+import Chess from "../src/S17/index.jsx";
+import Game from "../src/S17/lib/knight.js";
 import ReactGame from "react-gameboard/lib/component";
 
 const KN = ReactGame(Game);
 
-storiesOf("Knight", module)
+storiesOf("Knight (S17)", module)
   .add("Easy", () => (
     <KN row={3} col={4}>
-      <Chess/>
+      <Chess />
     </KN>
   ))
   .add("Impossible", () => (
     <KN row={8} col={8}>
-      <Chess/>
+      <Chess />
     </KN>
   ));
