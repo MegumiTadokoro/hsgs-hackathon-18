24f94596f125b937956ed9dad189bd57ad8ff824
Natsu Kagami <natsukagami@gmail.com>
2018-06-09 09:58:01 +0700

    Use more generic webpack override

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,23 @@
 const path = require("path");
 
+[].some;
+
 module.exports = (baseConfig, env, defaultConfig) => {
   // Extend defaultConfig as you need.
-
   const cssRule = defaultConfig.module.rules
     .find(
       v =>
         v.use instanceof Array &&
-        v.use.includes(
-          "/home/natsukagami/Projects/hsgs-hackathon-18/node_modules/style-loader/index.js"
+        v.use.some(
+          item =>
+            typeof item === "string" &&
+            item.endsWith("node_modules/style-loader/index.js")
         )
     )
     .use.find(
       v =>
-        v.loader ===
-        "/home/natsukagami/Projects/hsgs-hackathon-18/node_modules/css-loader/index.js"
+        typeof v.loader === "string" &&
+        v.loader.endsWith("node_modules/css-loader/index.js")
     );
   cssRule.options.modeules = true;
   console.log("Patched CSS Loader to use import modules");
